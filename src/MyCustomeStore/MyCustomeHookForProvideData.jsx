8fd31cbca5acb990ed32a2a5cd4c2a3b2c6ac1ba
Riaz115 <165409821+Riaz115@users.noreply.github.com>
2025-01-02 20:14:54 +0500

    Update MyCustomeHookForProvideData.jsx

--- src/MyCustomeStore/MyCustomeHookForProvideData.jsx
+++ src/MyCustomeStore/MyCustomeHookForProvideData.jsx
@@ -14,7 +14,7 @@ export const MyAllDataProvider = ({ children }) => {
   const [addText, setAddText] = useState(null);
 
   //this is my backend url
-  const myUrl = "https://fabolu-interview-backend-cm27.vercel.app/api";
+  const myUrl = "http://localhost:8000/api";
 
   //this is function for getting all profile users
   const forGettingAllUsers = async () => {
