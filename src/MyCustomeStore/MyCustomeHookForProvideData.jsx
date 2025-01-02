ce8951fd8d436bb5d5dc4db254784090a4db8554
Riaz115 <165409821+Riaz115@users.noreply.github.com>
2025-01-02 19:50:41 +0500

    Update MyCustomeHookForProvideData.jsx

--- src/MyCustomeStore/MyCustomeHookForProvideData.jsx
+++ src/MyCustomeStore/MyCustomeHookForProvideData.jsx
@@ -14,7 +14,7 @@ export const MyAllDataProvider = ({ children }) => {
   const [addText, setAddText] = useState(null);
 
   //this is my backend url
-  const myUrl = "http://localhost:3000";
+  const myUrl = "http://localhost:8000/api";
 
   //this is function for getting all profile users
   const forGettingAllUsers = async () => {
